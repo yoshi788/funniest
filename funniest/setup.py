7ec02cbd6a32747cbe0f11e72342efd8304f273e
Josh Elvidge <joshelvidge88@gmail.com>
2017-04-27 10:17:44 -0500

    First Change

--- funniest/setup.py
+++ funniest/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/yoshi788/funniest',
       author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      author_email='joshelvidge88@example.com',
       license='MIT',
       packages=['funniest'],
       zip_safe=False)
